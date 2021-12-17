fc445e5e49234e6bd46965bb554041bd07e61f09
danny p <danny@dannys-MacBook-Pro.local>
2021-12-17 17:26:47 -0500

    login/logout started

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,31 +1,15 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { States, Covid } = require('../models');
+const { States, Covid, User } = require('../models');
 
 router.get('/', (req, res) => {
   console.log(req.session);
-  Covid.findAll({
+  States.findAll({
     attributes: [
       'id',
-      'state',
-      'cases',
-      'deaths',
-      //[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      'state'
     ],
-   /* include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
-          model: User,
-          attributes: ['username']
-        }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]*/
+    
   })
     .then(dbPostData => {
       // pass a single post object into the homepage template
@@ -38,13 +22,13 @@ router.get('/', (req, res) => {
     });
 });
 
-/*router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
     return;
   }
 
   res.render('login');
-}); */
+});
 
 module.exports = router;
