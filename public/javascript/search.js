aa4bb0a521042cb063f45bfc27ce1f214b3d5e59
danny p <danny@dannys-MacBook-Pro.local>
2021-12-18 23:32:07 -0500

    search working

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,45 +1,4 @@
 
-/*const stateInput = document.querySelector('#search-state');
-const resultsContainer = document.querySelector('#result');
-const stateCont = document.querySelector('#state-cont');
-
-var submitHandler = function(event) {
-    event.preventDefault();
-    
-    var stateEl = stateInput.value.trim();
-    
-    
-
-
-    if (stateEl) {
-         getState(stateEl);
-        stateInput.value = ""; 
-        
-
-    } else {
-        alert("Please enter a valid state");
-
-        return;
-    }
-};
-
-async function getState(state) {
-    if (state) {
-        const response = await fetch(`/api/${state}`, {
-          method: 'get',
-          body: JSON.stringify({
-            state_name
-          }),
-          headers: { 'Content-Type': 'application/json' }
-          // headers: { 'Content-Type': 'application/json' }
-        });
-    
-      
-      }
-
-    
-};
-*/
 
 
 async function submitHandler(event) {
@@ -49,15 +8,23 @@ async function submitHandler(event) {
     
   
     if (state) {
-      const response = await fetch('/api/states/' + state, {
+     await fetch('/api/states/' + state, {
         method: 'get',
-      });
+        
+      }).then(response => {
+        if(response.ok){
+            return response.json();  
+        }
+          throw new Error('Request failed!');
+      }, networkError => {
+        console.log(networkError.message);
+      }).then(jsonResponse => {
+        console.log(jsonResponse);
+      }) 
+
+
   
-      if (response.ok) {
-        console.log(state);
-      } else {
-        alert(response.statusText);
-      }
+     
     }
   }
 
