4213c42996cbd286d7b9eaf5c24e7c2ac3c3223d
danny p <danny@dannys-MacBook-Pro.local>
2021-12-20 20:34:14 -0500

    commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,9 +6,20 @@ const { States, Covid, User } = require('../models');
 
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.render('search');
-  } else res.render('login')
-  
+    res.redirect('/search');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/search');
+    return;
+  }
+
+  res.render('signup');
 });
 
 
@@ -32,7 +43,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       // pass a single post object into the homepage template
       const states = dbPostData.map(post => post.get({ plain: true }));
-      res.render('homepage', { states });
+      res.render('homepage', { 
+        states,
+        
+       });
     })
     .catch(err => {
       console.log(err);
